db5e48d1bd15b98b5a3eebe7634d32786b854b75
mkouvara <marinoskouvaras@gmail.com>
2025-12-29 21:42:01 +0200

    swagger

--- API/handlers/image_http_handler.go
+++ API/handlers/image_http_handler.go
@@ -103,7 +103,6 @@ func (h *ImageHTTPHandler) UploadAvatar(w http.ResponseWriter, r *http.Request) 
 // @Accept       multipart/form-data
 // @Produce      json
 // @Param        post_id  formData  string  true  "ID of the post"
-// @Param        image    formData  file    true  "Image file(s) to upload" collectionFormat(multi)
 // @Success      200      {object}  ImageUploadResponse
 // @Failure      403      {object}  models.ErrorResponse
 // @Router       /api/v1/images/upload [post]

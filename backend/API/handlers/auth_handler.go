ebf868db088871a15b66be9d8e3acb40b50422a7
arsentsn <tsnarsen@gmail.com>
2026-03-05 08:22:46 +0200

    safari login bug fixed

--- backend/API/handlers/auth_handler.go
+++ backend/API/handlers/auth_handler.go
@@ -416,7 +416,7 @@ func (h *AuthHandler) createUserSession(w http.ResponseWriter, r *http.Request, 
 		Path:     "/",
 		Expires:  session.ExpiresAt,
 		HttpOnly: true,
-		Secure:   true,
+		Secure:   utils.CookieSecureEnabled(),
 		SameSite: http.SameSiteLaxMode,
 	})
 
